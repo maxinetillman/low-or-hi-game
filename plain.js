ccfe7fc3a0ec176c1e0e3b69855df65e6934415f
Tao Sharma <tao@schoolofcode.co.uk>
2021-04-09 17:41:24 +0100

    searches

--- plain.js
+++ plain.js
@@ -1,5 +1,9 @@
 // RANDOM BETWEEN 1 AND 10 -> SET AS x
 
+// Search: 'get random number js', 'save number in js', 'mdn js how to use same code'
+
+// let x = Math.floor(Math.random() * 10) + 1;
+
 function getRandomNumber() {
   return Math.floor(Math.random() * 10) + 1;
 }
@@ -8,13 +12,17 @@ let x = getRandomNumber();
 
 // PRINT x
 
+// Search 'how to print in console'
+
 function print(thing) {
   console.log(thing);
 }
 
 print(x);
 
-// USER INPUT guess (“higher” OR “lower”)
+// USER INPUT BOX guess (“higher” OR “lower”)
+
+// Search 'get user input box'
 
 function getUserInput(message) {
   return prompt(message);
@@ -28,13 +36,19 @@ while (guess !== "higher" && guess !== "lower") {
 
 // RANDOM BETWEEN 1 AND 10 -> SET AS y
 
+// repeated logic
+
 let y = getRandomNumber();
 
 // PRINT y
 
+// repeated logic
+
 print(y);
 
 // IF x > y AND guess EQUALS “lower” OR IF x < y AND guess EQUALS “higher”
+
+// search 'if one thing or another thing js'
 
 if (x > y && guess === "lower") {
   print("winner");
